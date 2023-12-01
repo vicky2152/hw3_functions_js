6ba2cfbb1f91c390b7c06d0538f234ce598f8d37
Vicky Naumenko <vickynaumenko@gmail.com>
2023-12-01 15:41:17 +0200

    Homework3 with function practice

---
+++ functions_js.js
@@ -0,0 +1,81 @@
+//Gas station for cars with different fuel types and diff sizes//
+
+//1
+
+let carFuel = "gas"
+let carSize = "S"
+
+function stationFunc() {
+    console.log( carFuel + ' = ' + carSize)    
+}
+stationFunc()
+
+//2
+
+carFuel = "gas"
+carSize = "M"
+
+function stationFunc() {
+    console.log(carFuel + ' = ' + carSize)
+}
+stationFunc()
+
+//3
+
+carFuel = "gas"
+carSize = "L"
+
+function stationFunc() {
+    console.log(carFuel + ' = ' + carSize)
+}
+stationFunc()
+
+//4
+
+carFuel = "gas"
+carSize = "XL"
+
+function stationFunc() {
+    console.log(carFuel + ' = ' + carSize)
+}
+stationFunc()
+
+//5
+
+carFuel = "diesel"
+carSize = "S"
+
+function stationFunc() {
+    console.log(carFuel + ' = ' + carSize)
+}
+stationFunc()
+
+//6
+
+carFuel = "diesel"
+carSize = "M"
+
+function stationFunc() {
+    console.log(carFuel + ' = ' + carSize)
+}
+stationFunc()
+
+//7
+
+carFuel = "diesel"
+carSize = "L"
+
+function stationFunc() {
+    console.log(carFuel + ' = ' + carSize)
+}
+stationFunc()
+
+//8
+
+carFuel = "diesel"
+carSize = "XL"
+
+function stationFunc() {
+    console.log(carFuel + ' = ' + carSize)
+}
+stationFunc()
